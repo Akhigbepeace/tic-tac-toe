3195a8828370f5dbe99d4ca68994d1e9a27b8952
Akhigbepeace <akhigbepeace2005@gmail.com>
2023-08-02 09:37:33 +0100

    Initial Commit and setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,21 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'dark-primary': '#31C3BD',
+        'light-primary': '#65E9E4',
+        'dark-secondary': '#F2B137',
+        'light-secondary': '#FFC860',
+        'dark-transparent': '#1A2A33',
+        'light-transparent': '#1F3641',
+        'dark-tertiary': '#A8BFC9',
+        'light-tertiary': '#DBE8ED',
+      },
+
+      fontFamily: {
+        outfit: ['Outfit', 'sans-serif'],
+      },
+
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
